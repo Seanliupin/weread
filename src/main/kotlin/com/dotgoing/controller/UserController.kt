6a5efb086999fdd56e22cb0a36f9b65e495a17dc
Sean <mingyi_127@126.com>
2018-01-31 18:34:32 +0800

    refactor: rename

--- src/main/kotlin/com/dotgoing/controller/UserController.kt
+++ src/main/kotlin/com/dotgoing/controller/UserController.kt
@@ -1,7 +1,7 @@
 package com.dotgoing.controller
 
 import com.dotgoing.model.User
-import com.dotgoing.repository.PersonRepository
+import com.dotgoing.repository.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 import java.util.*
@@ -11,27 +11,27 @@ import java.util.*
 class UserController {
 
     @Autowired
-    private val personRepository: PersonRepository? = null
+    private val userRepository: UserRepository? = null
 
     @PostMapping("/create")
     fun create(): Long? {
         val p = User("first -1 " + System.currentTimeMillis(), "last")
-        personRepository?.save(p)
-        val x = personRepository!!.save(p)
+        userRepository?.save(p)
+        val x = userRepository!!.save(p)
         return x.id
     }
 
     @GetMapping("/{userId}")
     fun info(@PathVariable userId: Long): User {
-        return personRepository!!.findOne(userId)
+        return userRepository!!.findOne(userId)
     }
 
     @RequestMapping("/all")
     fun persons(): List<User> {
-        personRepository!!.findAll()
+        userRepository!!.findAll()
 
         val j = ArrayList<User>()
-        personRepository.findAll().forEach { person -> j.add(person) }
+        userRepository.findAll().forEach { person -> j.add(person) }
 
         return j
 
--- src/main/kotlin/com/dotgoing/repository/UserRepository.kt
+++ src/main/kotlin/com/dotgoing/repository/UserRepository.kt
@@ -4,6 +4,6 @@ import com.dotgoing.model.User
 import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.data.repository.query.Param
 
-interface PersonRepository : PagingAndSortingRepository<User, Long> {
+interface UserRepository : PagingAndSortingRepository<User, Long> {
     fun findByLastName(@Param("name") name: String): List<User>
 }
