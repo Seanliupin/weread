c5bde9ee7dbc63bce315d85c8ad87fe481d774eb
Sean <mingyi_127@126.com>
2018-01-31 16:34:43 +0800

    add repository interface. its easy. up to now, emmbedded database was used

--- src/main/java/com/dotgoing/repository/PersonRepository.java
+++ src/main/java/com/dotgoing/repository/PersonRepository.java
@@ -1,4 +1,12 @@
 package com.dotgoing.repository;
 
-public class PersonRepository {
+import com.dotgoing.model.Person;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import java.util.List;
+import org.springframework.data.repository.query.Param;
+
+public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
+
+    List<Person> findByLastName(@Param("name") String name);
+
 }
